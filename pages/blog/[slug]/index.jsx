806c8dca22edbd4b7ccfc9f7e5f459ef16223021
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-06-08 11:34:51 +0200

    ...

--- pages/blog/[slug]/index.jsx
+++ pages/blog/[slug]/index.jsx
@@ -6,26 +6,10 @@ import Button from "@components/button";
 import Heading from "@components/heading";
 import BlogImageBanner from "@components/blog-image-banner";
 import useSWR from "swr";
-import { getPost, postsCacheKey } from "../../../api-routes/posts";
+import useSWRMutation from "swr/mutation"
 
-// const post = {
-//   id: "1234",
-//   title: "Why you should use a react framework",
-//   author: "John Doe",
-//   slug: "why-you-should-use-react-framework",
-//   createdAt: "2022-02-12",
-//   body: `
-//   <p>With the History extension the Editor will keep track of your changes. And if you think you made a mistake, you can redo your changes. Try it out, change the content and hit the undo button!</p>
-//   <p>And yes, you can also use a keyboard shortcut to undo changes (Control/Cmd Z) or redo changes (Control/Cmd Shift Z).</p>
-//   <p>Wow, this editor has support for links to the whole <a href="https://en.wikipedia.org/wiki/World_Wide_Web">world wide web</a>. We tested a lot of URLs and I think you can add *every URL* you want. Isn’t that cool? Let’s try <a href="https://statamic.com/">another one!</a> Yep, seems to work.</p>
-//   <p>By default every link will get a <code>rel="noopener noreferrer nofollow"</code> attribute. It’s configurable though.</p>
-//   <p><strong>This is bold.</strong></p>
-//   <p><u>This is underlined though.</u></p>
-//   <p><em>This is italic.</em></p>
-//   <p><s>But that’s striked through.</s></p>
-//   <p><code>This is code.</code></p>
-//   `,
-// };
+import { getPost, postsCacheKey, removePost } from "../../../api-routes/posts";
+
 
 export default function BlogPost() {
   const router = useRouter();
@@ -37,9 +21,13 @@ export default function BlogPost() {
     getPost({ slug })
   
   );
+
+  const {trigger: removeTrigger} = useSWRMutation(postsCacheKey, removePost)
+  // const removedPost = { id: post.id }
   
-  const handleDeletePost = () => {
+  const handleDeletePost = (_, {postId}) => {
     console.log({ id: post.id });
+    console.log({postId})
   };
   
   const handleEditPost = () => {
