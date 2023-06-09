832b941973b6fd92ce0381e64f6167ba71eeda06
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-06-09 13:09:54 +0200

    add get comments to posts

--- api-routes/comments.js
+++ api-routes/comments.js
@@ -1,4 +1,14 @@
-export const getComments = () => {
+import { supabase } from "../lib/supabaseClient";
+
+export const commentsCacheKey = "api/comments"
+
+export const getComments = async (postId) => {
+  const { data, error, status } = await supabase.
+    from("comments")
+    .select()
+    .eq("post_id", postId)
+
+  return { data, error, status };
   //Handle get all comments
 };
 
